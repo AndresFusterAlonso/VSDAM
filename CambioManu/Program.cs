7339002bedf74276fc975bba74aafa72b916fc44
Andres Fsuter <andresfusteralonso@gmail.com>
2020-11-06 13:50:39 +0100

    cambios en los repositorios oscar y manu

--- CambioManu/Program.cs
+++ CambioManu/Program.cs
@@ -7,6 +7,7 @@ namespace CambioManu
         static void Main(string[] args)
         {
             Console.WriteLine("Manu saluda");
+            int cambios = 2;
         }
     }
 }
--- proyecto_oscar/proyecto_oscar/Program.cs
+++ proyecto_oscar/proyecto_oscar/Program.cs
@@ -7,6 +7,7 @@ namespace proyecto_oscar
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int cambios = 2;
         }
     }
 }
